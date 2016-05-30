1a87a21ce297fdba4c80e0d579fe22cea71389eb
Leon Sackhoff <blackorpheussfdghdfnsd.dferasdf>
2016-05-30 17:11:43 +0200

    adfsfafawfafa

--- Client/src/menu/Menu.java
+++ Client/src/menu/Menu.java
@@ -7,6 +7,7 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 
 
@@ -15,18 +16,25 @@ public class Menu {
 	public Menu(){
 		JFrame f = new JFrame("Pacman Reloaded");
 		f.setBounds(0, 0, 1200, 900);
-		f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setLocationRelativeTo(null);
+		
 		
 		try {
 			final Image image1 = ImageIO.read(Menu.class.getResourceAsStream("/Background/background.png"));
 		
+		JPanel mainpanel = new JPanel();
+		mainpanel.setBounds(0, 0, 1200, 800);
+		f.add(mainpanel);
+			
 		JLabel background = new JLabel(new ImageIcon(image1));
 		background.setBounds(0, 0, 1200, 900);
+		mainpanel.add(background);
+		
 		
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 		}
-		
+		f.setVisible(true);
 	}
 	
 	public static void main(String[] args) {
