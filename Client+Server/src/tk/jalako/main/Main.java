1a4fb87e5a750e3f1eea3f4edfd93ca28bd13d37
Leon Sackhoff <blackorpheussfdghdfnsd.dferasdf>
2016-06-02 15:42:39 +0200

    asdasd

--- Client+Server/src/tk/jalako/main/Main.java
+++ Client+Server/src/tk/jalako/main/Main.java
@@ -1,13 +1,16 @@
 package tk.jalako.main;
 import javax.swing.JFrame;
 
+import menu.Menu;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		Game game = new Game();
-		JFrame frame = new JFrame("RPG");
+		JFrame frame = new JFrame("Pacman Reloaded");
 		frame.add(game);
+		frame.setIconImage(Menu.icon);
 		frame.pack();
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
